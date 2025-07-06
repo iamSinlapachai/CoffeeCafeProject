5c40986be392de1fc37afff0b35897c2ac7b6a2a
iamSinlapachai <s6752d10003@sau.ac.th>
2025-07-06 13:53:19 +0700

    Add project files.

---
+++ CoffeeCafeProject/FrmMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CoffeeCafeProject
+{
+    public partial class FrmMenu : Form
+    {        
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
